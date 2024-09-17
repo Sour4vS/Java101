3fd5655479912f30f2e4cc046913a81809dca088
Sour4vS <sour4vS@gmail.com>
2024-09-17 09:25:27 +0530

    Add files via upload

---
+++ Simplecalc.java
@@ -0,0 +1,37 @@
+package sour4v.java;
+
+import java.util.Scanner;
+
+public class Simplecalc {
+    public static void main(String[] args) {
+        int operation, n1,n2;
+        System.out.println("\n 1 - addition \n 2 - Subtraction \n 3 - Multiplication \n 4 - Division");
+        System.out.println("choose an operation");
+        Scanner sc = new Scanner(System.in);
+        operation = sc.nextInt();
+        System.out.println("First num is");
+        n1 = sc.nextInt();
+        System.out.println("Second num is");
+        n2 = sc.nextInt();
+        int result = 0;
+        switch(operation) {
+            case 1:
+                result = n1 + n2;
+                break;
+            case 2:
+                result = n1 - n2;
+                break;
+            case 3:
+                result = n1 * n2;
+                break;
+            case 4:
+                result = n1 / n2;
+                break;
+
+            default :
+                System.out.println("Invalid operation");
+                break;
+        }
+        System.out.println(" result is " + result);
+    }
+}
